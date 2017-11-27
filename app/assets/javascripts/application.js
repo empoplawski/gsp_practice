8983f6056f2c002e47d8001972437c28b662a8b4
Elise Poplawski <elise.poplawski@gmail.com>
2017-11-26 18:39:00 -0600

    add elements for presentation with samle code

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,3 +12,116 @@
 //
 //= require rails-ujs
 //= require_tree .
+
+
+// ##rotate
+
+// TweenMax.to(".owl", 2, {rotation:60});
+
+
+
+
+// ##shift
+// TweenMax.to(".owl", 2, {left:600});
+
+
+
+
+// ##spin/change size
+
+// TweenMax.to(".owl", 2, {x:600, rotation: 720, scale: 0.5});
+
+
+
+
+// ## tween from
+
+// TweenMax.from(".owl", 0.5, {opactiy:0, scale: 0, ease:Bounce.easeOut});
+
+
+
+
+
+// // select multiple by class
+
+// TweenMax.from(".tree", 0.5, {opactiy:0, scale: 0, ease:Bounce.easeOut});
+
+
+
+
+
+// as individual calls
+
+// TweenMax.to(".e", 5, {
+//   left:0,
+//   backgroundColor: "#cccccc",
+//   padding: 20,
+//   backgroundColor: "white",
+//   borderRadius: 26
+//   });
+
+// TweenMax.to(".l", 5, {
+//   left:150,
+//   backgroundColor: "#cccccc",
+//   padding: 20,
+//   backgroundColor: "white",
+//   borderRadius: 26
+//   });
+
+// TweenMax.to(".i", 5, {
+//   left:300,
+//   backgroundColor: "#cccccc",
+//   padding: 20,
+//   backgroundColor: "white",
+//   borderRadius: 26
+//   });
+
+// TweenMax.to(".s", 5, {
+//   left:450,
+//   backgroundColor: "#cccccc",
+//   padding: 20,
+//   backgroundColor: "white",
+//   borderRadius: 26
+//   });
+
+// TweenMax.to(".ee", 5, {
+//   left:600,
+//   backgroundColor: "#cccccc",
+//   padding: 20,
+//   backgroundColor: "white",
+//   borderRadius: 26
+//   });
+
+
+
+// run together, show delay
+
+// TweenMax.to(".owl", 5, {left:600});
+
+// TweenMax.to(".ee", 5, {
+//   left:600,
+//   backgroundColor: "#cccccc",
+//   padding: 20,
+//   backgroundColor: "white",
+//   borderRadius: 26,
+//   delay: 6
+//   });
+
+
+
+
+// // stagger elements from same class
+
+// TweenMax.staggerFrom(".tree",  3, {x:600, rotation: 720}, 1);
+
+
+
+
+// fadeout
+
+// TweenMax.to(".owl, .ee", 2, {opacity: 0});
+
+
+
+
+
